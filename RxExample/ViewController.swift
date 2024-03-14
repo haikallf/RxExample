0a265c7265b5d273c7ed8b2b6c2785bc62f976c9
haikallf <haikallzrd@gmail.com>
2024-03-14 13:20:40 +0700

    init

---
+++ RxExample/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  RxExample
+//
+//  Created by Samir iOS on 14/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .green
+    }
+}
+
